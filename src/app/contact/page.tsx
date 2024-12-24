7bd22df9405acac142c84a26a52d3293ae693997
triptiarora8 <triptiarora345@gmail.com>
2024-12-24 15:56:33 +0530

    contactus

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import React, { FormEvent, useState } from 'react';
+import { BackgroundBeams } from '@/components/ui/background-beams';
+import { Button } from '@/components/ui/moving-border';
+
+function MusicSchoolContactUs() {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Submitted:', { email, message });
+  };
+
+  return (
+    <div className="min-h-full bg-black py-12 pt-36 ">
+      {' '}
+      <BackgroundBeams className=" w-full h-full z-0" />
+      
+      <div className="max-w-2xl mx-auto relative z-10">
+        {' '}
+        <h1 className="text-lg md:text-4xl text-center font-sans font-bold mb-8 text-white">
+          Contact Us
+        </h1>
+        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
+          If you have any questions about our courses we&apos;re here to help.
+        
+        </p>
+        <form onSubmit={handleSubmit} className="space-y-4 mt-4 sm:w">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Your email address"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full p-4 bg-neutral-950 placeholder:text-neutral-700"
+            required
+          />
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Your message"
+            className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full  p-4 bg-neutral-950 placeholder:text-neutral-700"
+            rows={5}
+            required
+          ></textarea>
+          <div className='flex justify-center'>
+          <Button
+            
+            className="px-6 py-2 rounded-lg bg-cyan-600 text-white font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+          >
+            Send Message
+          </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default MusicSchoolContactUs;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem} from "../components/ui/navbar-menu";
 import { cn } from "../lib/utils";
+import Link from "next/link";
 
 
 export default function Navbar({ className }: { className?: string }) {
@@ -9,8 +10,7 @@ export default function Navbar({ className }: { className?: string }) {
   return (
     <div className={cn("fixed dark top-10  inset-x-0 max-w-2xl mx-auto z-50 ", className)}>
       <Menu setActive={setActive} >
-        <MenuItem setActive={setActive} active={active} item="Home">
-        </MenuItem>
+        <Link href="/"><MenuItem setActive={setActive} active={active} item="Home"></MenuItem></Link>
         <MenuItem setActive={setActive} active={active} item="Services">
         <div className=" flex flex-col p-2 text-sm">
           <HoveredLink href="/">Web development</HoveredLink>
@@ -20,7 +20,10 @@ export default function Navbar({ className }: { className?: string }) {
 
         </div>
         </MenuItem>
+        <Link href={"/contact"}>
         <MenuItem setActive={setActive} active={active} item="Contact us"></MenuItem>
+        </Link>
+
       </Menu>
         </div>
   )
